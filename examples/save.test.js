2d40c710b69090f3f98073dabb8691d536f6eff9
Vojtěch Dubrovský <v.dubr@hotmail.com{template .feature.Text}v.dubr@hotmail.com>
2015-12-28 02:03:47 +0100

    fic projections

--- examples/save.test.js
+++ examples/save.test.js
@@ -1,4 +1,5 @@
 var MapitoKnDown = require('../')
 
 // var knDown = MapitoKnDown({id:602191,format:'kml',test:true}).save()
-var knDown = MapitoKnDown({id:602191,format:'shp',projection:'EPSG:5514'}).save()
+// var knDown = MapitoKnDown({id:617237,format:'shp',projection:'EPSG:5514'}).save()
+var knDown = MapitoKnDown({id:617237,format:'shp',projection:'EPSG:4326'}).save()
--- index.js
+++ index.js
@@ -107,9 +107,7 @@ function MapitoKnDown(options) {
    * @type {string}
    * @private
    */
-   this.krovak_ = '+proj=krovak +lat_0=49.5 +lon_0=24.83333333333333 ' +
-   '+alpha=0 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +towgs84=' +
-   '570.8,85.7,462.8,4.998,1.587,5.261,3.56 +units=m +no_defs';
+   this.krovak_ = '+proj=krovak +lat_0=49.5 +lon_0=24.83333333333333 +alpha=30.28813972222222 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +towgs84=542.5,89.2,456.9,5.517,2.275,5.516,6.96 +pm=greenwich +units=m +no_defs';
 
   /**
    * URL where is stored kn gml data in EPSG:5514
@@ -124,9 +122,8 @@ function MapitoKnDown(options) {
    * @private
    */
   this.outputSrs_ = this.krovak_;
-
-  if (options && options.projection) {
-    this.outputSrs_ = options.projection;
+  if (options && options.projection && (options.projection !== 'EPSG:5514')) {
+      this.outputSrs_ = options.projection;
   }
   console.log('Transform to projection: ' + this.outputSrs_);
 
